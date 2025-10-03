a97f782a349a86efb87b4bb1c95b5751361b1bca
chrizxzayax <chmolina629@hotmail.com>
2025-10-02 18:43:10 -0700

    trying a different set?

--- lab16.cpp
+++ lab16.cpp
@@ -11,35 +11,31 @@ class Colors {
 
 
     public:
-    Colors() : red(0), green(0), blue(0) {} 
 
-    Colors(int r, int g, int b) : red(r), green(g), blue(b) {}
-    void setRed(int r) { red = r; }
-    void setGreen(int g) { green = g; }
-    void setBlue(int b) { blue = b; }
+        Colors() : red(0), green(0), blue(0) {} 
+        Colors(int r, int g, int b) : red(r), green(g), blue(b) {}
 
-    // Getters? first time i use these
-    int getRed() const { return red; }
-    int getGreen() const { return green; }  
-    int getBlue() const { return blue; }
+        void setRed(int r) { red = r; }
+        void setGreen(int g) { green = g; }
+        void setBlue(int b) { blue = b; }
 
-    void print() const {
-        cout << left << setw(4) << red << " "
-             << left << setw(4) << green << " "
-             << left << setw(4) << blue << endl;
-    }
+        // Getters? first time i use these
+        int getRed() const { return red; }
+        int getGreen() const { return green; }  
+        int getBlue() const { return blue; }
+
+        void print() const {
+            cout << left << setw(4) << red << " "
+                << left << setw(4) << green << " "
+                << left << setw(4) << blue << endl;
+        }
 
 };
 
 int main() {
-    Colors color1; // default constructor
-    color1.setRed(255);
-    color1.setGreen(0);
-    color1.setBlue(0);
-
-    Colors color2(0, 255, 0); //constructor with parameters
-    Colors color3(0, 0, 255);
-    Colors color4(128, 128, 128); 
+    Colors defaultColor; // default constructor
+    Colors fullColor(255, 100, 50); //the constructor with parameters
+    
 
     Colors color5;
     color5.setRed(75);
@@ -50,10 +46,13 @@ int main() {
          << left << setw(4) << "G" << " "
          << left << setw(4) << "B" << endl;
     cout << "------------------" << endl;
-    color1.print();
-    color2.print();
-    color3.print();
-    color4.print();
+
+    //output the default colors
+    cout<< "default constructor: ";
+    defaultColor.print();
+    cout<< "constructor with parameters: ";
+    fullColor.print();
+    cout<< "setters: ";
     color5.print();// print the RGB values
 
     return 0;
